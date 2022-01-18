280ea3d51689ad9ea44ff9ab55d186f7f7ad5264
arturfil <arturfil@hotmail.com>
2022-01-18 01:26:43 -0500

    added connection string for production

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Text;
 using API.Data;
@@ -26,7 +27,27 @@ namespace API {
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services) {
       services.AddDbContext<StoreContext>(opt => {
-        opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        string connStr;
+        if (env == "Development") {
+          connStr = Configuration.GetConnectionString("DefaultConnection");
+          // opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        } else {
+          var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+          connUrl = connUrl.Replace("postgres://", string.Empty);
+          var pgUserPass = connUrl.Split("@")[0];
+          var pgHostPortDb = connUrl.Split("@")[1];
+          var pgHostPort = pgHostPortDb.Split("/")[0];
+          var pgDb = pgHostPortDb.Split("/")[1];
+          var pgUser = pgUserPass.Split(":")[0];
+          var pgPass = pgUserPass.Split(":")[1];
+          var pgHost = pgHostPort.Split(":")[0];
+          var pgPort = pgHostPort.Split(":")[1];
+          connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require; Trust Server Certificate=true";
+          opt.UseNpgsql(connStr);
+        }
+
+      
       });
       services.AddControllers();
       services.AddSwaggerGen(c => {
